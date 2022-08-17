bc845847400b21e61d86a49344f6f89264174f0c
mahmoudcode <74220922+mahmoud913@users.noreply.github.com>
2022-08-17 11:57:09 -0500

    no solution yet

--- src/server/routes/apiRouter.js
+++ src/server/routes/apiRouter.js
@@ -19,9 +19,21 @@ router.post('/tag', apiController.getItemByTag, (req, res) =>
   res.status(200).json(res.locals.data)
 );
 
+// POST request for user login
+router.post('/login', apiController.getUser, (req, res) => {
+  res.status(200).json(res.locals.data)//maybe a different res key?
+});
+
+// POST request for user signup
+
+router.post('/signup',apiController.createUser, (req,res) => {
+  (200).json(res.locals.data)//maybe a different res key?
+});
+
 // PATCH request for updating "claimed" status
 router.patch('/update-item', apiController.updateItem, (req, res) =>
   res.status(200).send('Item claimed')
 );
+//mahmoud post request to the login and post request to the sign up
 
 module.exports = router;
